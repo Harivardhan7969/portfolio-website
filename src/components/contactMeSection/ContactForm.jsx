0f1200e346cab14c53c9829db5870542a13dfc15
Harivardhan7969 <harivardhan7632@gmail.com>
2025-03-29 08:55:12 +0530

    email

--- src/components/contactMeSection/ContactForm.jsx
+++ src/components/contactMeSection/ContactForm.jsx
@@ -19,8 +19,8 @@ const ContactForm = () => {
   const sendEmail = (e) => {
     e.preventDefault();
     emailjs
-      .sendForm("service_ko3hmpt", "template_ahbmmqd", form.current, {
-        publicKey: "I6HAT5mUZH7WHabGE",
+      .sendForm("service_ko3hm", "template_ahbmm", form.current, {
+        publicKey: "I6HAT5mUZH7WHab",
       })
       .then(
         () => {
