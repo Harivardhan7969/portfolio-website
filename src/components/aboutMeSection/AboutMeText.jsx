73a18345c2c8032f4886a345c8b18a551c6fe66c
Harivardhan7969 <harivardhan7632@gmail.com>
2025-03-29 07:46:01 +0530

    portfolio

---
+++ src/components/aboutMeSection/AboutMeText.jsx
@@ -0,0 +1,26 @@
+import { Link } from "react-scroll";
+
+const AboutMeText = () => {
+  return (
+    <div className="flex flex-col md:items-start sm:items-center md:text-left sm:text-center">
+      <h2 className="text-6xl text-cyan mb-10">About Me</h2>
+      <p>
+        DevGnan bridges tech students—US, UK, and beyond—with pros for mentorship, job support, and real-world gigs. Skip job portals—get expert resume help, mock interviews, freelance work, and top-tier training in Java, .NET, Angular, PostgreSQL, MongoDB, Angular 18, SQL, and more. From Hyderabad with 10+ years of tech expertise, we power freshers with assignments, skills, and success!
+      </p>
+      <button className="border border-orange rounded-full py-2 px-4 text-lg flex gap-2 items-center mt-10 hover:bg-orange transition-all duration-500 cursor-pointer md:self-start sm:self-center">
+        <Link
+          spy={true}
+          smooth={true}
+          duration={500}
+          offset={-120}
+          to="projects"
+          className="cursor-pointer text-white hover:text-cyan transition-all duration-500"
+        >
+          My Projects
+        </Link>
+      </button>
+    </div>
+  );
+};
+
+export default AboutMeText;
